7cc92171ca4a93384e10f28a02fe2c7681f41e28
pay1t <118164915+pay1t@users.noreply.github.com>
2023-07-31 10:06:07 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xb67145e93cd2e74c04fa8e926bfdb4c365547a9e";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
